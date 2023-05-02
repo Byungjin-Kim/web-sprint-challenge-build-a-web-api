8298c136bd3a0a315e2126769b813c233b5199e2
Byungjin-Kim <composerjins@gmail.com>
2023-05-02 17:11:25 -0500

    wip

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,6 @@
 // add middlewares here related to actions
+
+
+
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,23 @@
 // Write your "projects" router here!
+const express = require('express');
+
+const Project = require('./projects-model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Project.get()
+        .then(pro => {
+            res.json(pro);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: 'Error to get Projects'
+            })
+        })
+})
+
+
+
+module.exports = router;
